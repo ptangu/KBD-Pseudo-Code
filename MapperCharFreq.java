1c95b35373fa9ecbe87da5ee3e2434c046084da9
ptangu <prashanthtc@yahoo.com>
2016-03-04 15:55:50 -0600

    Create MapperCharFreq.java

---
+++ MapperCharFreq.java
@@ -0,0 +1,43 @@
+
+public class MapperCharFreq {
+	Open the input file
+	Open a new output file
+	For each word in the file 
+	{
+		read the first letter
+		add the (letter converted to Caps, 1) to the file
+		Continue iterating
+	}
+	
+Sort the file;
+}
+
+The above Mapper should provide the out put in the following format for a sample line "Cat and the Hat"
+
+C,1
+A,1
+T,1
+A,1
+N,1
+D,1
+T,1
+H,1
+E,1
+H,1
+A,1
+T,1
+
+Sorted result will be
+
+A,1
+A,1
+A,1
+C,1
+D,1
+E,1
+H,1
+H,1
+N,1
+T,1
+T,1
+T,1
